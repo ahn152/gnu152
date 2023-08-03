53fb58152c61d195bc2eaa9d4748cb359bfc1b90
ahn152 <141287344+ahn152@users.noreply.github.com>
2023-08-03 09:36:22 +0900

    Add files via upload

---
+++ 실험.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace MultiColoredModernUI.Forms
+{
+    partial class FormDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.DB_panel6 = new System.Windows.Forms.Panel();
+            this.DB_label9 = new System.Windows.Forms.Label();
+            this.DB_label8 = new System.Windows.Forms.Label();
+            this.DB_panel3 = new System.Windows.Forms.Panel();
+            this.DB_label3 = new System.Windows.Forms.Label();
+            this.DB_label2 = new System.Windows.Forms.Label();
+            this.DB_panel5 = new System.Windows.Forms.Panel();
+            this.DB_label7 = new System.Windows.Forms.Label();
+            this.DB_label6 = new System.Windows.Forms.Label();
+            this.DB_panel4 = new System.Windows.Forms.Panel();
+            this.DB_label5 = new System.Windows.Forms.Label();
+            this.DB_label4 = new System.Windows.Forms.Label();
+            this.DB_timer1 = new System.Windows.Forms.Timer(this.components);
+            this.DB_panel2 = new System.Windows.Forms.Panel();
+            this.DB_tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.DB_textBox1 = new System.Windows.Forms.TextBox();
+            this.DB_label1 = new System.Windows.Forms.Label();
+            this.DB_panel1 = new System.Windows.Forms.Panel();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.DB_panel6.SuspendLayout();
+            this.DB_panel3.SuspendLayout();
+            this.DB_panel5.SuspendLayout();
+            this.DB_panel4.SuspendLayout();
+            this.DB_panel2.SuspendLayout();
+            this.DB_tableLayoutPanel1.SuspendLayout();
+            this.DB_panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DB_panel6
+            // 
+            this.DB_panel6.BackColor = System.Drawing.Color.MediumPurple;
+            this.DB_panel6.Controls.Add(this.DB_label9);
+            this.DB_panel6.Controls.Add(this.DB_label8);
+            this.DB_panel6.Location = new System.Drawing.Point(1152, 2);
+            this.DB_panel6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DB_panel6.Name = "DB_panel6";
+            this.DB_panel6.Size = new System.Drawing.Size(308, 67);
+            this.DB_panel6.TabIndex = 0;
+            // 
+            // DB_label9
+            // 
+            this.DB_label9.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.DB_label9.AutoSize = true;
+            this.DB_label9.Font = new System.Drawing.Font("굴림", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_label9.Location = new System.Drawing.Point(180, 25);
+            this.DB_label9.Name = "DB_label9";
+            this.DB_label9.Size = new System.Drawing.Size(112, 35);
+            this.DB_label9.TabIndex = 0;
+            this.DB_label9.Text = "label2";
+            // 
+            // DB_label8
+            // 
+            this.DB_label8.AutoSize = true;
+            this.DB_label8.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_label8.Location = new System.Drawing.Point(12, 7);
+            this.DB_label8.Name = "DB_label8";
+            this.DB_label8.Size = new System.Drawing.Size(91, 16);
+            this.DB_label8.TabIndex = 0;
+            this.DB_label8.Text = "달성률[%]";
+            // 
+            // DB_panel3
+            // 
+            this.DB_panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.DB_panel3.Controls.Add(this.DB_label3);
+            this.DB_panel3.Controls.Add(this.DB_label2);
+            this.DB_panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DB_panel3.Location = new System.Drawing.Point(3, 2);
+            this.DB_panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DB_panel3.Name = "DB_panel3";
+            this.DB_panel3.Size = new System.Drawing.Size(377, 67);
+            this.DB_panel3.TabIndex = 0;
+            // 
+            // DB_label3
+            // 
+            this.DB_label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.DB_label3.AutoSize = true;
+            this.DB_label3.Font = new System.Drawing.Font("굴림", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_label3.Location = new System.Drawing.Point(214, 25);
+            this.DB_label3.Name = "DB_label3";
+            this.DB_label3.Size = new System.Drawing.Size(112, 35);
+            this.DB_label3.TabIndex = 0;
+            this.DB_label3.Text = "label2";
+            // 
+            // DB_label2
+            // 
+            this.DB_label2.AutoSize = true;
+            this.DB_label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_label2.Location = new System.Drawing.Point(10, 7);
+            this.DB_label2.Name = "DB_label2";
+            this.DB_label2.Size = new System.Drawing.Size(115, 16);
+            this.DB_label2.TabIndex = 0;
+            this.DB_label2.Text = "일일 목표수량";
+            // 
+            // DB_panel5
+            // 
+            this.DB_panel5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.DB_panel5.Controls.Add(this.DB_label7);
+            this.DB_panel5.Controls.Add(this.DB_label6);
+            this.DB_panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DB_panel5.Location = new System.Drawing.Point(769, 2);
+            this.DB_panel5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DB_panel5.Name = "DB_panel5";
+            this.DB_panel5.Size = new System.Drawing.Size(377, 67);
+            this.DB_panel5.TabIndex = 0;
+            // 
+            // DB_label7
+            // 
+            this.DB_label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.DB_label7.AutoSize = true;
+            this.DB_label7.Font = new System.Drawing.Font("굴림", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_label7.Location = new System.Drawing.Point(217, 25);
+            this.DB_label7.Name = "DB_label7";
+            this.DB_label7.Size = new System.Drawing.Size(112, 35);
+            this.DB_label7.TabIndex = 0;
+            this.DB_label7.Text = "label2";
+            // 
+            // DB_label6
+            // 
+            this.DB_label6.AutoSize = true;
+            this.DB_label6.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_label6.Location = new System.Drawing.Point(8, 7);
+            this.DB_label6.Name = "DB_label6";
+            this.DB_label6.Size = new System.Drawing.Size(163, 16);
+            this.DB_label6.TabIndex = 0;
+            this.DB_label6.Text = "시간당 생산량(UPH)";
+            // 
+            // DB_panel4
+            // 
+            this.DB_panel4.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.DB_panel4.Controls.Add(this.DB_label5);
+            this.DB_panel4.Controls.Add(this.DB_label4);
+            this.DB_panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DB_panel4.Location = new System.Drawing.Point(386, 2);
+            this.DB_panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DB_panel4.Name = "DB_panel4";
+            this.DB_panel4.Size = new System.Drawing.Size(377, 67);
+            this.DB_panel4.TabIndex = 0;
+            // 
+            // DB_label5
+            // 
+            this.DB_label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.DB_label5.AutoSize = true;
+            this.DB_label5.Font = new System.Drawing.Font("굴림", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_label5.Location = new System.Drawing.Point(213, 25);
+            this.DB_label5.Name = "DB_label5";
+            this.DB_label5.Size = new System.Drawing.Size(112, 35);
+            this.DB_label5.TabIndex = 0;
+            this.DB_label5.Text = "label2";
+            // 
+            // DB_label4
+            // 
+            this.DB_label4.AutoSize = true;
+            this.DB_label4.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_label4.Location = new System.Drawing.Point(12, 7);
+            this.DB_label4.Name = "DB_label4";
+            this.DB_label4.Size = new System.Drawing.Size(115, 16);
+            this.DB_label4.TabIndex = 0;
+            this.DB_label4.Text = "주간 목표수량";
+            // 
+            // DB_panel2
+            // 
+            this.DB_panel2.Controls.Add(this.DB_tableLayoutPanel1);
+            this.DB_panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DB_panel2.Location = new System.Drawing.Point(0, 85);
+            this.DB_panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DB_panel2.Name = "DB_panel2";
+            this.DB_panel2.Size = new System.Drawing.Size(1534, 71);
+            this.DB_panel2.TabIndex = 9;
+            // 
+            // DB_tableLayoutPanel1
+            // 
+            this.DB_tableLayoutPanel1.ColumnCount = 4;
+            this.DB_tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.DB_tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.DB_tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.DB_tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.DB_tableLayoutPanel1.Controls.Add(this.DB_panel3, 0, 0);
+            this.DB_tableLayoutPanel1.Controls.Add(this.DB_panel4, 1, 0);
+            this.DB_tableLayoutPanel1.Controls.Add(this.DB_panel5, 2, 0);
+            this.DB_tableLayoutPanel1.Controls.Add(this.DB_panel6, 3, 0);
+            this.DB_tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DB_tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.DB_tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DB_tableLayoutPanel1.Name = "DB_tableLayoutPanel1";
+            this.DB_tableLayoutPanel1.RowCount = 1;
+            this.DB_tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.DB_tableLayoutPanel1.Size = new System.Drawing.Size(1534, 71);
+            this.DB_tableLayoutPanel1.TabIndex = 1;
+            // 
+            // DB_textBox1
+            // 
+            this.DB_textBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.DB_textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DB_textBox1.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_textBox1.Location = new System.Drawing.Point(27, 13);
+            this.DB_textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DB_textBox1.Name = "DB_textBox1";
+            this.DB_textBox1.Size = new System.Drawing.Size(187, 25);
+            this.DB_textBox1.TabIndex = 1;
+            this.DB_textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DB_label1
+            // 
+            this.DB_label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.DB_label1.AutoSize = true;
+            this.DB_label1.Font = new System.Drawing.Font("굴림", 33F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.DB_label1.Location = new System.Drawing.Point(647, 23);
+            this.DB_label1.Name = "DB_label1";
+            this.DB_label1.Size = new System.Drawing.Size(289, 44);
+            this.DB_label1.TabIndex = 0;
+            this.DB_label1.Text = "Dash  Board";
+            // 
+            // DB_panel1
+            // 
+            this.DB_panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.DB_panel1.Controls.Add(this.DB_textBox1);
+            this.DB_panel1.Controls.Add(this.DB_label1);
+            this.DB_panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DB_panel1.Location = new System.Drawing.Point(0, 0);
+            this.DB_panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DB_panel1.Name = "DB_panel1";
+            this.DB_panel1.Size = new System.Drawing.Size(1534, 85);
+            this.DB_panel1.TabIndex = 8;
+            // 
+            // chart2
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(0, 156);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(1534, 437);
+            this.chart2.TabIndex = 11;
+            this.chart2.Text = "chart1";
+            // 
+            // FormDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1534, 593);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.DB_panel2);
+            this.Controls.Add(this.DB_panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormDashboard";
+            this.Text = "Dash board";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormDashboard_Load);
+            this.DB_panel6.ResumeLayout(false);
+            this.DB_panel6.PerformLayout();
+            this.DB_panel3.ResumeLayout(false);
+            this.DB_panel3.PerformLayout();
+            this.DB_panel5.ResumeLayout(false);
+            this.DB_panel5.PerformLayout();
+            this.DB_panel4.ResumeLayout(false);
+            this.DB_panel4.PerformLayout();
+            this.DB_panel2.ResumeLayout(false);
+            this.DB_tableLayoutPanel1.ResumeLayout(false);
+            this.DB_panel1.ResumeLayout(false);
+            this.DB_panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel DB_panel6;
+        private System.Windows.Forms.Label DB_label9;
+        private System.Windows.Forms.Label DB_label8;
+        private System.Windows.Forms.Panel DB_panel3;
+        private System.Windows.Forms.Label DB_label3;
+        private System.Windows.Forms.Label DB_label2;
+        private System.Windows.Forms.Panel DB_panel5;
+        private System.Windows.Forms.Label DB_label7;
+        private System.Windows.Forms.Label DB_label6;
+        private System.Windows.Forms.Panel DB_panel4;
+        private System.Windows.Forms.Label DB_label5;
+        private System.Windows.Forms.Label DB_label4;
+        private System.Windows.Forms.Timer DB_timer1;
+        private System.Windows.Forms.Panel DB_panel2;
+        private System.Windows.Forms.TableLayoutPanel DB_tableLayoutPanel1;
+        private System.Windows.Forms.TextBox DB_textBox1;
+        private System.Windows.Forms.Label DB_label1;
+        private System.Windows.Forms.Panel DB_panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
---
+++ 실험.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Collections;
+
+
+namespace MultiColoredModernUI.Forms
+{
+    public partial class FormDashboard : Form
+    {
+         string connectionString = "Data Source=222.235.141.8,1111; Initial Catalog=GNU23_04; User ID=GNU2304; Password=1234";
+
+        public FormDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDashboard_Load(object sender, EventArgs e)
+        {
+
+
+            // 데이터베이스 연결 문자열
+            string connectionString = "Data Source=222.235.141.8,1111; Initial Catalog=GNU23_04; User ID=GNU2304; Password=1234";
+
+            #region <달성률 구하기>
+            // SQL 쿼리문 
+            string query2 = "SELECT SUM(ProductionQuantity) AS TotalProductionQuantity, SUM(ProductionGoal) AS TotalProductionGoal FROM EquipmentPerformance";
+
+            // 달성률 변수
+            double achievementRate = 0;
+
+            // SQL Server 연결과 데이터 조회
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query2, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // 총 생산량과 총 목표량을 가져옴
+                            int totalProductionQuantity = Convert.ToInt32(reader["TotalProductionQuantity"]);
+                            int totalProductionGoal_ = Convert.ToInt32(reader["TotalProductionGoal"]);
+
+                            // 달성률 계산
+                            if (totalProductionGoal_ != 0)
+                                achievementRate = (totalProductionQuantity / (double)totalProductionGoal_) * 100;
+                        }
+                    }
+                }
+            }
+            #endregion
+
+            // DB_label9에 달성률 표시
+            DB_label9.Text = achievementRate.ToString("0.00") + "%";
+
+            #region <아래 그리드>
+            //DataTable table = new DataTable();
+
+            //table.Columns.Add("작업장", typeof(string));
+            //table.Columns.Add("상태", typeof(string));
+            //table.Columns.Add("공정정보", typeof(string));
+            //table.Columns.Add("설비온도", typeof(string));
+            //table.Columns.Add("전압", typeof(string));
+            //table.Columns.Add("목표수량(일일)", typeof(string));
+            //table.Columns.Add("목표수량(전체)", typeof(string));
+            //table.Columns.Add("생산수량", typeof(string));
+            //table.Columns.Add("달성률[%]", typeof(string));
+
+            //DB_dataGridView1.DataSource = table;
+            #endregion
+
+            #region <주간 목표 수량>
+            // 쿼리문 작성
+            string query = "SELECT ProductionQuantity, Funiq FROM EquipmentPerformance";
+
+            // 데이터를 담을 DataTable
+            DataTable dataTable = new DataTable();
+
+            // SQL Server 연결과 데이터 조회
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataTable);
+                }
+            }
+            // 그래프 그리기
+            //DrawBarChart(dataTable);
+
+            // SQL 쿼리문
+            string query1 = "SELECT ProductionGoal FROM EquipmentPerformance";
+
+            // 합계를 저장할 변수
+            int totalProductionGoal = 0;
+
+            // SQL Server 연결과 데이터 조회
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query1, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // 데이터베이스에서 가져온 값을 더함
+                            int productionGoal = reader.GetInt32(0);
+                            totalProductionGoal += productionGoal;
+                        }
+                    }
+                }
+            }
+
+            // DB_label5에 결과를 표시
+            DB_label5.Text = totalProductionGoal.ToString();
+
+            #endregion
+
+            DataTable dt = new DataTable();
+
+           
+
+            DataTable dt220 = GetDataFromDatabase();
+            DrawChart(dt220);
+
+           
+        }
+
+        private int myPrivateVariable;
+       
+        public  DataTable GetDataFromDatabase()
+        {
+            DataTable dt220 = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                    connection.Open();
+                string query22 = "SELECT MLINECODE, SUM(WGOOD) TotalWGOOD, SUM(WBAD) TotalWBAD FROM Result GROUP BY MLINECODE;";
+                using (SqlCommand command = new SqlCommand(query22, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dt220);
+                    }
+                }
+            }
+
+            return dt220;
+        }
+
+        private void DrawChart(DataTable dataTable)
+        {
+
+            
+            this.Controls.Add(chart2);
+
+            ChartArea chartArea = new ChartArea();
+            chartArea.AxisX.Title = "설비";
+            chartArea.AxisY.Title = "Defect Rate (%)";
+            chart2.ChartAreas.Add(chartArea);
+
+            Series series = new Series("DefectRate");
+            series.ChartType = SeriesChartType.Column;
+            chart2.Series.Add(series);
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                string equipment = row["MLINECODE"].ToString();
+                // int totalProductionQuantity = Convert.ToInt32(row["TotalProductionQuantity"]);
+                int TotalWGOOD = Convert.ToInt32(row["TotalWGOOD"]);
+                int TotalWBAD = Convert.ToInt32(row["TotalWBAD"]);
+
+                //불량품 수량 / (양품 수량 + 불량품 수량) * 100
+                double defectRate = (double)TotalWBAD / (TotalWGOOD+ TotalWBAD) * 100.0;
+
+                chart2.Series["DefectRate"].Points.AddXY(equipment, defectRate);
+            }
+        }
+
+        
+    }
+
+       
+    }
+
+          
+
+
+           
+
